d5a1787b91afe79983455103e1b8a6b42eeb454f
Farida Bayoumy <117514847+faridaaismaill12@users.noreply.github.com>
2024-11-28 16:08:04 +0200

    Update chat-gateway.ts

--- Backend/src/modules/communication/chat-gateway.ts
+++ Backend/src/modules/communication/chat-gateway.ts
@@ -3,27 +3,39 @@ SubscribeMessage,
 WebSocketGateway,
 OnGatewayConnection,
 OnGatewayDisconnect, 
-WebSocketServer} from '@nestjs/websockets'; 
+WebSocketServer,
+MessageBody} from '@nestjs/websockets'; 
 import { Socket, Server } from 'socket.io';
+import { chatDocument } from './schemas/chat-schema';
 
 @WebSocketGateway(3002, {})
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer()
     server!: Server;
 
-handleConnection(client: Socket) {
+    
+
+
+
+handleConnection(client: Socket) { // we need to log the connection
     console.log("New client connected", client.id); 
+
+    this.server.emit('user-joined', {
+        message: `New user joined the Chat': ${client.id},`
+    });
 }
 
-handleDisconnect(client: Socket) {
+handleDisconnect(client: Socket) { // we need to log the disconnection
     console.log("Client disconnected", client.id);
-}
 
-    // we need to listen to the message event
+    this.server.emit('user-left', {
+        message: `User left the Chat': ${client.id},`
+    });
+}
+// this method is used to make sure that every client in a groupchat sees that message
     @SubscribeMessage('newMessage')
-    handleNewMessage(client: Socket, message: any) {
-        // we need to log the message
-        console.log( message);
+    handleNewMessage(@MessageBody() message: string) {
+        this.server.emit('newMessage', message); // we need to broadcast the message to all clients   
     }
 }
 
