57d76bffbb6e09d49f8bffd5bb8d54bc2dc31156
ammarhassona <ammarhassona15@gmail.com>
2024-11-30 00:43:33 +0200

    idk

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,14 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseConfig } from './config/database.config';
 // import { CartModule } from './modules/cart/cart.module';
 import { CommunicationModule } from './modules/communication/communication.module';
-// import { UserModule } from './modules/user/user.module';
+import { UserModule } from './modules/user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true, // Loads .env file globally
         }),
+        MongooseModule.forRoot(process.env.MONGO_URI!),
         DatabaseConfig,
+        UserModule,
         // CartModule,
         CommunicationModule,
         // UserModule,
