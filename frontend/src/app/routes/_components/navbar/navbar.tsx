4148d899f655d134ed6f70b9d15c97b1a2dc5380
AHegab <amhegab305@gmail.com>
2024-12-12 15:13:22 +0200

    Update navbar.tsx

--- frontend/src/app/routes/_components/navbar/navbar.tsx
+++ frontend/src/app/routes/_components/navbar/navbar.tsx
@@ -2,14 +2,9 @@ import React from 'react'
 
 const navbar = () => {
   return (
-    <header>
-        <div className='navbar-container'>
-            <div className="logo">
-                <h1>BananaBread</h1>
-            </div>
-
-        </div>
-    </header>
+    <div>
+      
+    </div>
   )
 }
 
