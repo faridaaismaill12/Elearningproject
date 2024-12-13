9ad3d2264d785d264036decc189caf346c968a68
maryamkashif197 <147558318+maryamkashif197@users.noreply.github.com>
2024-12-12 17:25:43 -0800

    Base Content
    Added the navbar , default profile pictures, and more to it if I missed out on anything

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,19 @@
-
+"use client"
+import Image from "next/image";
 import Layout from "./routes/layout";
+import Home from "./routes/home/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export default function Home() {
+export default function App() {
+  const shouldShowSidebar = !location.pathname.startsWith('/messages');
   return (
-    <div className="flex max-w-8xl mx-auto">
-      <Layout>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is some content inside the layout.</p>
-      </Layout>
-    </div>
+    <Router>
+    <Layout>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Layout>
+  </Router>
   );
 }
 
