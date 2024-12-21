f7e6a716bd13ce987f323e5d77d300d68838ad2f
AHegab <amhegab305@gmail.com>
2024-12-21 17:07:21 +0200

    Revert "Merge remote-tracking branch 'origin/clara2.0' into Ammar"
    This reverts commit 1751f76cc53d50ef118d29d216c841b31883e868, reversing
    changes made to bf199b60134e9383051822543ebd3f917dfd2bc7.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,14 +4,6 @@
 import React from "react";
 import Layout from "./routes/layout";
 import Home from "./routes/home/HomePage";
-import Login from "./authentication/login/page";
-import RegisterPage from "./user/register/page";
-import LoginPage from "./user/login/page";
-import ForgotPasswordPage from "./user/forgotpassword/page";
-import DeleteMyProfile from "./user/deletemyprofile/page";
-import Logout from "./user/logout/page";
-import UpdateProfile from "./user/updatemyprofile/page";
-import ViewMyProfile from "./user/viewmyprofile/page";
 // import "./app.css";
 
 
@@ -19,7 +11,6 @@ export default function App() {
   return (
     <Layout>
       <Home />
-      
     </Layout>
   );
 }
